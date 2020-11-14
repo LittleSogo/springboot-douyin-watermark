c07ddb32b76da078e363ff55dd9bff4b46ee6990
slix <847034331@qq.com>
2020-11-14 22:06:31 +0800

    第7提交

---
+++ src/main/java/com/xiaofu/douyin/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.xiaofu.douyin.controller;
+
+@Controller
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("--------------");
+    }
+}
